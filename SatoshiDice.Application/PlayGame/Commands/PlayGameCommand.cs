64fbfa146c586a05d0f2e504bc456aba597d2208
TChukwuleta <tobechichukwuleta125@gmail.com>
2022-10-15 20:41:03 +0100

    play command

--- SatoshiDice.Application/PlayGame/Commands/PlayGameCommand.cs
+++ SatoshiDice.Application/PlayGame/Commands/PlayGameCommand.cs
@@ -18,7 +18,7 @@ namespace SatoshiDice.Application.PlayGame.Commands
         public decimal Amount { get; set; }
         public int MaximimNumberGuess { get; set; }
         public int MinimumNumberGuess { get; set; }
-        public bool GameInsured { get; set; }
+        /*public bool GameInsured { get; set; }*/
         public string UserId { get; set; }
     }
 
@@ -42,6 +42,7 @@ namespace SatoshiDice.Application.PlayGame.Commands
                 if (player == null) return Result.Failure("Invalid active player");
                 var minimumDiceNumber = _config["MinimumDiceNumber"];
                 var maximumDiceNumber = _config["MaximumDiceNumber"];
+                var gameOwnerUserId = _config["GameOwnerId"];
                 if(request.MaximimNumberGuess > int.Parse(maximumDiceNumber)) return Result.Failure("Maximum number guess greater than guess limit. Please guess a number between 1 and 100");
                 if (request.MinimumNumberGuess < int.Parse(minimumDiceNumber)) return Result.Failure("Minimum number guess less than guess limit. Please guess a number between 1 and 100");
                 if (request.MaximimNumberGuess > int.Parse(maximumDiceNumber) && request.MinimumNumberGuess < int.Parse(minimumDiceNumber)) return Result.Failure("Both guess numbers out of range");
@@ -55,17 +56,21 @@ namespace SatoshiDice.Application.PlayGame.Commands
                     Amount = request.Amount,
                     TransactionReference = uniqueId,
                     TransactionType = TransactionType.Debit,
-                    TransactionStatus = TransactionStatus.Success
+                    TransactionStatus = TransactionStatus.Processing,
+                    Status = Status.Active
                 });
                 transactionRequests.Add(new Transaction
                 {
-                    UserId = "gameowner userid",
+                    UserId = gameOwnerUserId,
                     CreatedDate = DateTime.Now,
                     Amount = request.Amount,
                     TransactionReference = uniqueId,
                     TransactionType = TransactionType.Credit,
-                    TransactionStatus = TransactionStatus.Success
+                    TransactionStatus = TransactionStatus.Processing,
+                    Status = Status.Active
                 });
+                await _context.Transactions.AddRangeAsync(transactionRequests);
+                await _context.SaveChangesAsync(cancellationToken);
                 decimal winAmount = request.Amount;
                 decimal rate = default;
                 var guessDiff = request.MaximimNumberGuess - request.MinimumNumberGuess;
@@ -74,80 +79,141 @@ namespace SatoshiDice.Application.PlayGame.Commands
                     case 0:
                         break;
                     case int i when i > 0 && i <= 10:
-                        rate = winAmount * (decimal)0.9;
-                        winAmount += rate;
+                        rate = winAmount * (decimal)0.04;
                         break;
-                    case int i when i > 10 && i <= 10:
-                        rate = winAmount * (decimal)0.8;
-                        winAmount += rate;
+                    case int i when i > 10 && i <= 20:
+                        rate = winAmount * (decimal)0.03;
                         break;
                     case int i when i > 20 && i <= 30:
-                        rate = winAmount * (decimal)0.7;
-                        winAmount += rate;
+                        rate = winAmount * (decimal)0.02;
                         break;
                     case int i when i > 30 && i <= 40:
-                        rate = winAmount * (decimal)0.6;
-                        winAmount += rate;
+                        rate = winAmount * (decimal)0.01;
                         break;
                     case int i when i > 40 && i <= 50:
-                        rate = winAmount * (decimal)0.5;
-                        winAmount += rate;
                         break;
                     case int i when i > 50 && i <= 60:
-                        rate = winAmount * (decimal)0.4;
-                        winAmount += rate;
                         break;
                     case int i when i > 60 && i <= 70:
-                        rate = winAmount * (decimal)0.3;
-                        winAmount += rate;
                         break;
                     case int i when i > 70 && i <= 80:
-                        rate = winAmount * (decimal)0.2;
-                        winAmount += rate;
                         break;
                     case int i when i > 80 && i <= 90:
-                        rate = winAmount * (decimal)0.1;
-                        winAmount += rate;
                         break;
                     case int i when i > 90 && i <= 100:
-                        return Result.Failure("Guess margin too wide. Kindly select a lower margin");
+                        break;
                     default:
                         break;
                 }
+                // Perform Bitcoin transaction
+                bool transactionSuccess = false;
+                // If transaction success, update transaction status from processing to completed
+                if (transactionSuccess)
+                {
+                    var transactions = await _context.Transactions.Where(c => c.TransactionReference == uniqueId).ToListAsync();
+                    if(transactions.Count > 0)
+                    {
+                        foreach (var item in transactions)
+                        {
+                            item.TransactionStatus = TransactionStatus.Success;
+                            _context.Transactions.Update(item);
+                        }
+                        await _context.SaveChangesAsync(cancellationToken);
+                    }
+                }
+                else
+                {
+                    var transactions = await _context.Transactions.Where(c => c.TransactionReference == uniqueId).ToListAsync();
+                    if (transactions.Count > 0)
+                    {
+                        player.Balance += request.Amount;
+                        foreach (var item in transactions)
+                        {
+                            item.TransactionStatus = TransactionStatus.Failed;
+                            _context.Transactions.Update(item);
+                        }
+                        await _context.SaveChangesAsync(cancellationToken);
+                    }
+                    return Result.Failure("An error occured while performing the bitcoin transaction");
+                }
                 Random rand = new Random();
                 int gameNumber = rand.Next(1, 100);
-                if (request.MinimumNumberGuess >= gameNumber && request.MaximimNumberGuess <= gameNumber)
+                if (request.MinimumNumberGuess == gameNumber || request.MaximimNumberGuess == gameNumber)
                 {
-                    player.Balance += winAmount;
-                    _context.Players.Update(player);
-                    transactionRequests.Add(new Transaction
+                    var successfulTransaction = new List<Transaction>();
+                    // Perform Bitcoin transaction
+                    // If transaction success, update transaction status from processing to completed
+                    if (transactionSuccess)
                     {
-                        UserId = request.UserId,
-                        CreatedDate = DateTime.Now,
-                        Amount = winAmount,
-                        TransactionReference = uniqueId,
-                        TransactionType = TransactionType.Credit,
-                        TransactionStatus = TransactionStatus.Success
-                    });
-                    transactionRequests.Add(new Transaction
+                        player.Balance += (winAmount + rate);
+                        _context.Players.Update(player);
+                        successfulTransaction.Add(new Transaction
+                        {
+                            UserId = request.UserId,
+                            CreatedDate = DateTime.Now,
+                            Amount = winAmount,
+                            TransactionReference = uniqueId,
+                            TransactionType = TransactionType.Credit,
+                            TransactionStatus = TransactionStatus.Success
+                        });
+                        successfulTransaction.Add(new Transaction
+                        {
+                            UserId = gameOwnerUserId,
+                            CreatedDate = DateTime.Now,
+                            Amount = winAmount,
+                            TransactionReference = uniqueId,
+                            TransactionType = TransactionType.Debit,
+                            TransactionStatus = TransactionStatus.Success
+                        });
+                        await _context.Transactions.AddRangeAsync(transactionRequests);
+                        await _context.SaveChangesAsync(cancellationToken);
+                    }
+                    else
                     {
-                        UserId = "gameowner userid",
-                        CreatedDate = DateTime.Now,
-                        Amount = winAmount,
-                        TransactionReference = uniqueId,
-                        TransactionType = TransactionType.Debit,
-                        TransactionStatus = TransactionStatus.Success
-                    });
-                    await _context.Transactions.AddRangeAsync(transactionRequests);
-                    await _context.SaveChangesAsync(cancellationToken);
+                        return Result.Failure("An error occured while performing the bitcoin transaction");
+                    }
                     var response = new
                     {
                         AmountWon = winAmount,
                         DiceValue = gameNumber
                     };
-                    return Result.Success("Yaaayyyyyy... You win");
+                    return Result.Success("Yaaayyyyyy... You win", response);
                 }
-                if (request.GameInsured)
+                if(rate > 0)
+                {
+                    var successfulTransaction = new List<Transaction>();
+                    // Perform Bitcoin transaction
+                    // If transaction success, update transaction status from processing to completed
+                    if (transactionSuccess)
+                    {
+                        player.Balance += (winAmount + rate);
+                        _context.Players.Update(player);
+                        successfulTransaction.Add(new Transaction
+                        {
+                            UserId = request.UserId,
+                            CreatedDate = DateTime.Now,
+                            Amount = winAmount,
+                            TransactionReference = uniqueId,
+                            TransactionType = TransactionType.Credit,
+                            TransactionStatus = TransactionStatus.Success
+                        });
+                        successfulTransaction.Add(new Transaction
+                        {
+                            UserId = gameOwnerUserId,
+                            CreatedDate = DateTime.Now,
+                            Amount = winAmount,
+                            TransactionReference = uniqueId,
+                            TransactionType = TransactionType.Debit,
+                            TransactionStatus = TransactionStatus.Success
+                        });
+                        await _context.Transactions.AddRangeAsync(successfulTransaction);
+                    }
+                    else
+                    {
+                        return Result.Failure("An error occured while performing the bitcoin transaction");
+                    }
+                }
+                /*if (request.GameInsured)
                 {
                     if (request.MinimumNumberGuess - gameNumber <= 10 || gameNumber - request.MaximimNumberGuess <= 10)
                     {
@@ -181,7 +247,8 @@ namespace SatoshiDice.Application.PlayGame.Commands
                         DiceValue = gameNumber
                     };
                     return Result.Success("Opps... Please play again", response);
-                }
+                }*/
+                await _context.Transactions.AddRangeAsync(transactionRequests);
                 await _context.SaveChangesAsync(cancellationToken);
                 var failedResponse = new
                 {
